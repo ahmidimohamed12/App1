a7db889403674ec0850e996bd1555149c6ad53e7
Ahmidi Mohamed <ahmidinador123@gmail.com>
2019-10-20 12:25:10 +0200

    GearFast

---
+++ App1/App1/Models/Commands.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    public class Commands
+    {
+        public int id { get; set; }
+        public string cmd { get; set; }
+
+    }
+}
---
+++ App1/App1/Models/Resturants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1.Models
+{
+    public class Resturants
+    {
+        public int id { get; set; }
+        public string  name{ get; set; }
+        public Commands cmd { get; set; }
+        public int qt;
+        public int total { get; set; }
+    }
+}
